65eb42c8b813139cf2ca2d67650382af7b09679f
Afsana Khan <afsana.291094@gmail.com>
2023-10-04 15:05:13 +0200

    Update host.py
    updated codes

--- host.py
+++ host.py
@@ -1,37 +1,52 @@
+from typing import List, Tuple
 import numpy as np
-from LR import model
+from model import LogisticRegressionModel
 from client_interface import ClientInterface
 
 class Host(ClientInterface):
-    def __init__(self,lr,model:model,data):
+    """
+    Implementation of a passive party (Host) in a vertical federated learning system.
+    """
+    def __init__(self, lr: float, model_instance: LogisticRegressionModel, data: np.ndarray):
+        """
+        Initializes the Host instance.
+
+        Parameters:
+        lr (float): Learning rate.
+        model_instance (model): Instance of the logistic regression model.
+        data (np.ndarray): Data for the host.
+        """
         self.lr = lr
         self.x = data
-        self.model = model(self.x,self.lr)
-        self.z= None
+        self.model = model_instance(self.x, self.lr)
+        self.z = None
         self.diff = None
 
-    def create_batch(self,ids):
-
+    def create_batch(self, ids: List[int]) -> np.ndarray:
+        """Creates a batch of data using the provided ids."""
         return np.array([self.x[id] for id in ids])
 
-    def forward(self,ids):
-        x=self.create_batch(ids)
+    def forward(self, ids: List[int]):
+        """Performs a forward pass using the data associated with the provided ids."""
+        x = self.create_batch(ids)
         self.z = self.model.forward(x)
 
-    def receive(self,diff):
-        #self.dw,self.db=grad
+    def receive(self, diff: np.ndarray):
+        """Receives diff from the active party (Guest)."""
         self.diff = diff
-    
-    def send(self):
+
+    def send(self) -> np.ndarray:
+        """Sends computed z to the active party (Guest)."""
         return self.z
 
     def compute_gradient(self):
-         self.dw,self.db = self.model.compute_gradient(self.diff)
-    
-    def update_model(self):
+        """Computes the gradient based on received diff and local parameters."""
+        self.dw, self.db = self.model.compute_gradient(self.diff)
 
+    def update_model(self):
+        """Updates the model parameters."""
         self.model.update_model()
 
-    
-
-    
+    def compute_contribution(self, X_host):
+            # X_host is the input features available to the Host
+            return np.dot(X_host, self.model.w) + self.model.b
